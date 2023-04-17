54627ccb44fea8d295f1bd1a6b42b188f3687a80
SaiVenkataGiriPrasad <41141894+SaiVenkataGiriPrasad@users.noreply.github.com>
2023-04-17 21:41:22 +0000

    written dict obj

---
+++ creating_dataset/dictionary_object.py
@@ -0,0 +1,51 @@
+import random 
+import time
+names = ['SAI', 'UDAY', 'BHARATH', 'RISHIKA', 'LAHARI', 'PRAVALLIKA', 'RAMYA', 'VIDYA', 'SOWRYA', 'NIKHIL', 'MANI', 'JACK', 'HIRAL', 'TOM CRUISE', 'VISHNU', 'VENKAT', 'RACHANA', 'PRUTHVI', 'GIREESH', 'LASYA', 'SOWMINI']
+
+
+def create_dataset():
+    file_c = open('generate_dataset.txt', 'w', encoding = 'UTF-8')
+    create_rows = 5000000 # Number of rows creating in a file
+    for line in range(create_rows):
+        generate_data = random.choice(names)
+        file_c.write(generate_data + '\n')
+    file_c.close()
+
+
+def read_dataset_list():
+    names_count_list = []
+    for name in names: #append the list with zero, referencing with indexes of names.
+        names_count_list.append(0)
+    with open('generate_dataset.txt', 'r', encoding='UTF-8') as file_r:
+        for name in file_r:
+            name = name.strip()
+            if name != '':
+                names_count_list[names.index(name)] += 1
+    print(names_count_list)
+
+
+def read_dataset_dict():
+    names_count_dict = {}
+    for name in names:
+        names_count_dict[name] = 0 # Mapping value(zero) with key(name)
+    with open('generate_dataset.txt', 'r', encoding = 'UTF-8') as file_r:
+        for name in file_r:
+            name = name.strip()
+            if name != '':
+                names_count_dict[name] += 1
+    print(names_count_dict)
+
+# By comparing the two methods(list & dict), you'll be able to see dictionary is significantly faster than the list.
+# Time taking for list objects
+create_dataset()
+
+t0 = time.time()
+read_dataset_list()
+t1 = time.time()
+print("TIME TAKEN FOR THE LIST {}".format(t1-t0))
+
+# Time taking for dict objects 
+t2 = time.time()
+read_dataset_dict()
+t3 = time.time()
+print("TIME TAKEN FOR THE DICT {}".format(t2-t3))
